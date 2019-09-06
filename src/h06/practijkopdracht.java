af0ad1d3dc5ad98988fa1021c1a3b2d80c61d305
jm181 <jmeijndert@roc-dev.com>
2019-09-06 14:28:14 +0100

    opdracht eind hoofdstuk 6

--- src/h06/practijkopdracht.java
+++ src/h06/practijkopdracht.java
@@ -6,24 +6,25 @@ import java.awt.*;
 import java.applet.*;
 
 public class practijkopdracht extends Applet {
-    int a;
-    int b;
-    int c;
-    int d;
-    int uitkomst;
+    double a;
+    double b;
+    double c;
+    double uitkomst;
 
 
     public void init() {
-        a= (int) 5.9;
-        b= (int) 6.3;
-        c= (int) 6.9;
-        d= 3;
-        uitkomst= (int) (a +b +c) / d;
+        a= 5.9;
+        b= 6.3;
+        c= 6.9;
+        uitkomst= (a + b + c ) / 3;
+        uitkomst = uitkomst * 10;
+        uitkomst = (int) uitkomst;
+        uitkomst = uitkomst / 10;
 
     }
 
     public void paint(Graphics g) {
-        g.drawString("het grmiddelde cijfer van de cursist is:"+uitkomst, 50, 50 );
+        g.drawString("het grmiddelde cijfer van de cursist is: " + uitkomst, 50, 50 );
 
     }
 
