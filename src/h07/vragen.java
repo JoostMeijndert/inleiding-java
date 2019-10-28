68e0d0c7b7cfa0720c70ab6b6369ab52c6365541
jm181 <jmeijndert@roc-dev.com>
2019-10-28 09:00:22 +0100

    vragen H7

---
+++ src/h07/vragen.java
@@ -0,0 +1,20 @@
+package h07;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class vragen extends Applet{
+
+    @Override
+    public void init() {
+
+        // 1. nee je kan de javaAp niet vinden in deze methode
+        // 2. in de init classe
+        // 3. set backgroundcolor
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+    }
+}
